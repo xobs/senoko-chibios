e37e159a22f8c8610bbd50a015ab787b64686f4c
Sean Cross <xobs@kosagi.com>
2013-09-23 18:11:23 +0800

    bionic: Add memcpy function

--- projects/novena-pmb/bionic.h
+++ projects/novena-pmb/bionic.h
@@ -4,4 +4,5 @@ size_t _strspn(const char *s1, const char *s2);
 char * _strpbrk(const char *s1, const char *s2);
 char *_strtok(char *str, const char *delim, char **saveptr);
 int _strcasecmp(const char *s1, const char *s2);
+void *_memcpy(void *dst0, const void *src0, size_t length);
 #endif /* __BIONIC_H__ */
