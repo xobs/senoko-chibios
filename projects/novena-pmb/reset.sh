6ac06ee7f3ff6a65365d7a9f211f516ee27dccec
Sean Cross <xobs@kosagi.com>
2013-09-23 18:11:47 +0800

    reset: Bring the PMB out of programming mode upon reset

--- projects/novena-pmb/reset.sh
+++ projects/novena-pmb/reset.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Bring board out of "program" mode
+echo 148 > /sys/class/gpio/export 2> /dev/null
+echo out > /sys/class/gpio/gpio148/direction 2> /dev/null
+echo 0 > /sys/class/gpio/gpio148/value 2> /dev/null
+
+
 # Bring the board out of reset
 echo 149 > /sys/class/gpio/export 2> /dev/null
 echo out > /sys/class/gpio/gpio149/direction 2> /dev/null
