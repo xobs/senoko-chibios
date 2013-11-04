c0fa9355f0c7cae2db87688aa8c68b125256a4c5
Sean Cross <xobs@kosagi.com>
2013-11-04 15:01:45 +0800

    support scripts: Use "sudo" for non-root access
    Development is no longer being done as the root user.  Therefore, wrap
    the GPIO calls around sudo, to allow non-root users to run the scripts.

--- projects/kosagi-senoko/flashmode.sh
+++ projects/kosagi-senoko/flashmode.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Bring board out of "program" mode
+# Put board into "reprogram" mode
 sudo /bin/sh -c 'echo 148 > /sys/class/gpio/export 2> /dev/null'
 sudo /bin/sh -c 'echo out > /sys/class/gpio/gpio148/direction 2> /dev/null'
-sudo /bin/sh -c 'echo 0 > /sys/class/gpio/gpio148/value 2> /dev/null'
+sudo /bin/sh -c 'echo 1 > /sys/class/gpio/gpio148/value 2> /dev/null'
 
-
-# Bring the board out of reset
+# Reset board
 sudo /bin/sh -c 'echo 149 > /sys/class/gpio/export 2> /dev/null'
 sudo /bin/sh -c 'echo out > /sys/class/gpio/gpio149/direction 2> /dev/null'
 sudo /bin/sh -c 'echo 0 > /sys/class/gpio/gpio149/value 2> /dev/null'
 sleep 1
 sudo /bin/sh -c 'echo 1 > /sys/class/gpio/gpio149/value 2> /dev/null'
-
